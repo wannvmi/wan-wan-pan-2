170318febc620ca12ed77a644834fd74e35b5c49
wannvmi <wannvmi@163.com>
2021-10-18 00:01:20 +0800

    add

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,7 +1,10 @@
-import {createApp} from 'vue';
+// 通用字体
+import 'vfonts/Lato.css';
+// 等宽字体
+import 'vfonts/FiraCode.css';
+
+import { createApp } from 'vue';
 import App from '/@/App.vue';
 import router from '/@/router';
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+createApp(App).use(router).mount('#app');
